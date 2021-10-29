aaff972cecec2e38a8eb2c909b8f85bc47cd2311
catupperman <catupperman@gmail.com>
2021-10-29 17:48:28 -0400

    username password mutations in resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,16 +1,40 @@
 const { Book, User } = require('../models');
-const { AuthenticationError } =require('apollo-server-express');
-const { signToken }=require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-            if(context.user){
-                const userData = await User.findOne({_id: context.user._id}).select('-__v -password');
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
                 return userData;
             }
             throw new AuthenticationError("Not logged in!");
         }
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('User not found')
+            }
+
+            const corretPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('Incorrect Password');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        }
+
     }
 }
 
